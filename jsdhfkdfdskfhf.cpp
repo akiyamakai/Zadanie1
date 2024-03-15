23fb868a0bb6006f7913d4436ada113e620e672a
akiyamakai <grusha.cs@gmail.com>
2024-03-15 08:49:24 +0200

    Добавьте файлы проекта.

---
+++ jsdhfkdfdskfhf.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main() {
+    const int SIZE = 5; // Розмір масиву
+    int arr[SIZE] = { 1, 2, 3, 4, 5 }; // Заданий масив
+
+    int sum = 0; // Змінна для збереження суми
+
+    // Обчислюємо суму всіх елементів масиву
+    for (int i = 0; i < SIZE; ++i) {
+        sum += arr[i];
+    }
+
+    // Виводимо результат
+    std::cout << "The sum of array elements: " << sum << std::endl;
+
+    return 0;
+}
